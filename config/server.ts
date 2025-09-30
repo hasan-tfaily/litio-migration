becb06ca699c4d63c6cef33ac33385bb140181a5
hasan-tfaily <hasan.tf.11@outlook.com>
2025-09-30 23:15:49 +0300

    changes

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", "http://localhost:1337"),
+  proxy: false, // set to true only when you put Nginx in front
+  app: { keys: env.array("APP_KEYS") },
 });
